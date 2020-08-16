94b97d931e4a9edd9b4a1c8bd20a176de367725c
Abhay Jirati <abhi.jrt12@gmail.com>
2020-08-17 00:03:42 +0530

    Updates

---
+++ precache-manifest.88c01271b8c1aa7f86ab4c8d50a96a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd34f17dab0e71bbfdeef5b0cabb58e",
+    "url": "/react-todo-list/index.html"
+  },
+  {
+    "revision": "155074b137e655a6709f",
+    "url": "/react-todo-list/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "2c34e1964442192a6e0a",
+    "url": "/react-todo-list/static/js/2.41a08886.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-list/static/js/2.41a08886.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "155074b137e655a6709f",
+    "url": "/react-todo-list/static/js/main.bba04304.chunk.js"
+  },
+  {
+    "revision": "2c6e5d8f9eaaedf40455",
+    "url": "/react-todo-list/static/js/runtime-main.f23babde.js"
+  },
+  {
+    "revision": "a2f89af4153c498bad28733a04f49bff",
+    "url": "/react-todo-list/static/media/my_image.a2f89af4.jpeg"
+  }
+]);
